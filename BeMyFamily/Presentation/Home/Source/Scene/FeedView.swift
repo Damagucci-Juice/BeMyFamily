8c9f8a59fdba8855a92e1a9859ef7ffcdb038731
Damagucci-Juice <jsr9874@gmail.com>
2025-12-27 18:38:11 +0900

    feat: Devider 를 마지막 동물이 아니면 표시

--- BeMyFamily/Presentation/Home/Source/Scene/FeedView.swift
+++ BeMyFamily/Presentation/Home/Source/Scene/FeedView.swift
@@ -45,18 +45,24 @@ struct FeedView: View {
     private var feedList: some View {
         LazyVStack(spacing: UIConstants.Spacing.interFeedItem) {
             ForEach(viewModel.animals) { animal in
-                NavigationLink(value: FeedRoute.detail(entity: animal)) {
-                    FeedItemView(animal: animal)
-                        .onAppear {
-                            if let last = viewModel.animals.last, animal.id == last.id {
-                                isReachedToBottom = true
-                                Task {
-                                    await viewModel.fetchAnimalsIfCan()
+                VStack {
+                    NavigationLink(value: FeedRoute.detail(entity: animal)) {
+                        FeedItemView(animal: animal)
+                            .onAppear {
+                                if let last = viewModel.animals.last, animal.id == last.id {
+                                    isReachedToBottom = true
+                                    Task {
+                                        await viewModel.fetchAnimalsIfCan()
+                                    }
                                 }
                             }
-                        }
+                    }
+                    .tint(.primary)
+
+                    if animal.id != viewModel.animals.last?.id {
+                        Divider()
+                    }
                 }
-                .tint(.primary)
             }
         }
     }
